5b0070dfe0ce1871ebfaed98809b2bf90965a384
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2019-01-04 17:19:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`AJ Server`,"http://twitch.tv/idk")
-client.user.setStatus("dnd")
 });
 
 
